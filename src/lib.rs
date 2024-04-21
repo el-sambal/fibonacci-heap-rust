6641512016d82370e60d56f790d13266fd6821f8
el-sambal <AronHardeman@proton.me>
2024-04-21 13:30:03 +0200

    make push() return ptr to inserted element

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,30 @@ pub struct FibonacciHeap<T> {
     min: *mut Node<T>,
 }
 
+pub struct Node<T> {
+    /// The key of the node.
+    key: T,
+    /// The left neighbor of this node in the circular doubly linked list.
+    ///
+    /// If this node is the only element in the circular doubly linked list, then this is a pointer
+    /// to the node itself.
+    left: *mut Node<T>,
+    /// The right neighbor of this node in the circular doubly linked list.
+    ///
+    /// If this node is the only element in the circular doubly linked list, then this is a pointer
+    /// to the node itself.
+    right: *mut Node<T>,
+    /// The parent of this node. This is a null pointer if the node has no parent.
+    parent: *mut Node<T>,
+    /// A pointer to *any* child of this node. This is a null pointer if the node has no children.
+    child: *mut Node<T>,
+    /// The number of nodes in the child list of this node.
+    degree: usize,
+    /// A boolean flag which is true if and only if this node has lost a child node since the last time
+    /// it was made the child of another node.
+    mark: bool,
+}
+
 impl<T: Ord> FibonacciHeap<T> {
     /// Construct a new, empty Fibonacci heap.
     pub const fn new() -> FibonacciHeap<T> {
@@ -44,7 +68,11 @@ impl<T: Ord> FibonacciHeap<T> {
     }
 
     /// Insert an element into the Fibonacci heap.
-    pub fn push(&mut self, item: T) {
+    ///
+    /// A raw pointer to the node containing the inserted element is returned. You can pass this
+    /// pointer into the `decrease_key` and `delete` methods. If you are not going to use these
+    /// methods on the inserted element, you can discard the pointer outputted by this method.
+    pub fn push(&mut self, item: T) -> *mut Node<T> {
         let node: *mut Node<T> = Box::into_raw(Box::new(Node {
             key: item,
             left: std::ptr::null_mut(),
@@ -69,6 +97,7 @@ impl<T: Ord> FibonacciHeap<T> {
             }
         }
         self.n += 1;
+        node
     }
 
     /// Extracts the minimum element from the Fibonacci heap and returns it.
@@ -213,30 +242,6 @@ impl<T> Drop for FibonacciHeap<T> {
             drop_recursive(self.min);
         }
     }
-}
-
-struct Node<T> {
-    /// The key of the node.
-    key: T,
-    /// The left neighbor of this node in the circular doubly linked list.
-    ///
-    /// If this node is the only element in the circular doubly linked list, then this is a pointer
-    /// to the node itself.
-    left: *mut Node<T>,
-    /// The right neighbor of this node in the circular doubly linked list.
-    ///
-    /// If this node is the only element in the circular doubly linked list, then this is a pointer
-    /// to the node itself.
-    right: *mut Node<T>,
-    /// The parent of this node. This is a null pointer if the node has no parent.
-    parent: *mut Node<T>,
-    /// A pointer to *any* child of this node. This is a null pointer if the node has no children.
-    child: *mut Node<T>,
-    /// The number of nodes in the child list of this node.
-    degree: usize,
-    /// A boolean flag which is true if and only if this node has lost a child node since the last time
-    /// it was made the child of another node.
-    mark: bool,
 }
 
 #[cfg(test)]
