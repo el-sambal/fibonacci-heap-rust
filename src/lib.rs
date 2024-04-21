bac287c7de55564e72c015d184cb892d12e1f20e
el-sambal <AronHardeman@proton.me>
2024-04-21 11:36:31 +0200

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 /// This is a min-Fibonacci heap.
 pub struct FibonacciHeap<T> {
-    /// The current number of nodes in the Fibonacci heap
+    /// The current number of nodes in the Fibonacci heap.
     n: usize,
     /// A pointer to the current minimal node in the Fibonacci heap. This is a null pointer if the
     /// heap is empty.
@@ -8,6 +8,7 @@ pub struct FibonacciHeap<T> {
 }
 
 impl<T: Ord> FibonacciHeap<T> {
+    /// Construct a new, empty Fibonacci heap.
     pub const fn new() -> FibonacciHeap<T> {
         FibonacciHeap {
             n: 0,
@@ -15,10 +16,13 @@ impl<T: Ord> FibonacciHeap<T> {
         }
     }
 
+    /// Checks whether the Fibonacci heap is empty.
     pub fn is_empty(&self) -> bool {
         !self.min.is_null()
     }
 
+    /// Produce a Fibonacci heap from melding two existing Fibonacci heaps. The two inputs are
+    /// consumed.
     pub fn from_meld(heap1: FibonacciHeap<T>, heap2: FibonacciHeap<T>) -> FibonacciHeap<T> {
         let mut heap = FibonacciHeap::<T>::new();
         if heap1.is_empty() {
@@ -39,33 +43,7 @@ impl<T: Ord> FibonacciHeap<T> {
         heap
     }
 
-    pub fn pop(&mut self) -> Option<T> {
-        let popped = self.min;
-        if !popped.is_null() {
-            unsafe {
-                let mut child = (*popped).child;
-                if !child.is_null() {
-                    while !(*child).parent.is_null() {
-                        (*child).parent = std::ptr::null_mut();
-                        child = (*child).right;
-                    }
-                }
-                FibonacciHeap::concatenate_circular_lists(child, popped);
-                if (*popped).right != popped {
-                    FibonacciHeap::remove_from_circular_list(popped, (*popped).right);
-                    self.min = (*popped).right;
-                    self.consolidate();
-                } else {
-                    self.min = std::ptr::null_mut();
-                }
-            }
-            self.n -= 1;
-            unsafe { Some(Box::from_raw(popped).key) }
-        } else {
-            None
-        }
-    }
-
+    /// Insert an element into the Fibonacci heap.
     pub fn push(&mut self, item: T) {
         let node: *mut Node<T> = Box::into_raw(Box::new(Node {
             key: item,
@@ -93,34 +71,32 @@ impl<T: Ord> FibonacciHeap<T> {
         self.n += 1;
     }
 
-    /// Adds a node to a circular doubly linked list. Both inputs must not be null pointers.
-    unsafe fn add_node_to_nonempty_circular_list(new_item: *mut Node<T>, list: *mut Node<T>) {
-        (*new_item).right = (*list).right;
-        (*new_item).left = list;
-        (*(*list).right).left = new_item;
-        (*list).right = new_item;
-    }
-
-    /// Concatenates two circular doubly linked lists.
-    unsafe fn concatenate_circular_lists(list1: *mut Node<T>, list2: *mut Node<T>) {
-        if list1.is_null() || list2.is_null() {
-            return;
+    /// Extracts the minimum element from the Fibonacci heap and returns it.
+    pub fn pop(&mut self) -> Option<T> {
+        let popped = self.min;
+        if !popped.is_null() {
+            unsafe {
+                let mut child = (*popped).child;
+                if !child.is_null() {
+                    while !(*child).parent.is_null() {
+                        (*child).parent = std::ptr::null_mut();
+                        child = (*child).right;
+                    }
+                }
+                FibonacciHeap::concatenate_circular_lists(child, popped);
+                if (*popped).right != popped {
+                    FibonacciHeap::remove_from_circular_list(popped);
+                    self.min = (*popped).right;
+                    self.consolidate();
+                } else {
+                    self.min = std::ptr::null_mut();
+                }
+            }
+            self.n -= 1;
+            unsafe { Some(Box::from_raw(popped).key) }
+        } else {
+            None
         }
-        let prev_list1_right = (*list1).right;
-        let prev_list2_left = (*list2).left;
-        (*list1).right = list2;
-        (*list2).left = list1;
-        (*prev_list1_right).left = prev_list2_left;
-        (*prev_list2_left).right = prev_list1_right;
-    }
-
-    /// Removes an element from a circular doubly linked list. The specified element must be a
-    /// member of this list, otherwise it is UB. `elem` and `list` should not be equal, otherwise
-    /// it is UB. This function does no freeing whatsoever. The node pointed to by `elem` is not
-    /// changed; its key and pointers stay intact.
-    unsafe fn remove_from_circular_list(elem: *const Node<T>, list: *mut Node<T>) {
-        (*(*elem).right).left = (*elem).left;
-        (*(*elem).left).right = (*elem).right;
     }
 
     /// This method basically fixes up the Fibonacci heap (it is called by the `pop()` method) such
@@ -158,7 +134,7 @@ impl<T: Ord> FibonacciHeap<T> {
                     // that
                     node_it = (*node_it).left;
                 }
-                FibonacciHeap::remove_from_circular_list(y, x);
+                FibonacciHeap::remove_from_circular_list(y);
                 (*x).degree += 1;
                 if !(*x).child.is_null() {
                     Self::add_node_to_nonempty_circular_list(y, (*x).child);
@@ -186,6 +162,34 @@ impl<T: Ord> FibonacciHeap<T> {
         }
         self.min = min;
     }
+
+    /// Adds a node to a circular doubly linked list. Both inputs must not be null pointers.
+    unsafe fn add_node_to_nonempty_circular_list(new_item: *mut Node<T>, list: *mut Node<T>) {
+        (*new_item).right = (*list).right;
+        (*new_item).left = list;
+        (*(*list).right).left = new_item;
+        (*list).right = new_item;
+    }
+
+    /// Concatenates two circular doubly linked lists.
+    unsafe fn concatenate_circular_lists(list1: *mut Node<T>, list2: *mut Node<T>) {
+        if list1.is_null() || list2.is_null() {
+            return;
+        }
+        let prev_list1_right = (*list1).right;
+        let prev_list2_left = (*list2).left;
+        (*list1).right = list2;
+        (*list2).left = list1;
+        (*prev_list1_right).left = prev_list2_left;
+        (*prev_list2_left).right = prev_list1_right;
+    }
+
+    /// Removes an element from a circular doubly linked list. This function does no freeing whatsoever.
+    /// The node pointed to by `elem` is not changed; its key and pointers stay intact.
+    unsafe fn remove_from_circular_list(elem: *const Node<T>) {
+        (*(*elem).right).left = (*elem).left;
+        (*(*elem).left).right = (*elem).right;
+    }
 }
 
 impl<T> Drop for FibonacciHeap<T> {
@@ -195,7 +199,7 @@ impl<T> Drop for FibonacciHeap<T> {
 }
 
 struct Node<T> {
-    /// The key of the node
+    /// The key of the node.
     key: T,
     /// The left neighbor of this node in the circular doubly linked list.
     ///
@@ -211,10 +215,10 @@ struct Node<T> {
     parent: *mut Node<T>,
     /// A pointer to *any* child of this node. This is a null pointer if the node has no children.
     child: *mut Node<T>,
-    /// The number of nodes in the child list of this node
+    /// The number of nodes in the child list of this node.
     degree: usize,
     /// A boolean flag which is true if and only if this node has lost a child node since the last time
-    /// it was made the child of another node
+    /// it was made the child of another node.
     mark: bool,
 }
 
@@ -305,11 +309,178 @@ mod tests {
         let mut prev = i32::min_value();
         let mut count = 0;
         while let Some(popped) = fh.pop() {
-            println!("popped! {popped}");
             assert!(popped >= prev);
             prev = popped;
             count += 1;
         }
         assert!(count == 1004);
+    }
+
+    #[test]
+    fn test_push_many_then_pop_many_with_many_duplicates() {
+        let mut fh: FibonacciHeap<i32> = FibonacciHeap::new();
+        for i in 0..1000 {
+            fh.push((i * i * i) % 300);
+        }
+        let mut prev = i32::min_value();
+        let mut count = 0;
+        while let Some(popped) = fh.pop() {
+            assert!(popped >= prev);
+            prev = popped;
+            count += 1;
+        }
+        assert!(count == 1000);
+    }
+
+    #[test]
+    fn test_push_many_then_pop_many_with_very_many_duplicates() {
+        let mut fh: FibonacciHeap<i32> = FibonacciHeap::new();
+        for i in 0..1000 {
+            fh.push((i * i * i) % 3);
+        }
+        let mut prev = i32::min_value();
+        let mut count = 0;
+        while let Some(popped) = fh.pop() {
+            assert!(popped >= prev);
+            prev = popped;
+            count += 1;
+        }
+        assert!(count == 1000);
+    }
+
+    #[test]
+    fn test_push_many_then_pop_many_test_input_equals_output() {
+        let mut fh: FibonacciHeap<i32> = FibonacciHeap::new();
+        let mut input: Vec<i32> = vec![];
+        let mut output: Vec<i32> = vec![];
+        for i in 0..1000 {
+            let val = (i * i * i) % 1500;
+            fh.push(val);
+            input.push(val);
+        }
+        let mut prev = i32::min_value();
+        let mut count = 0;
+        while let Some(popped) = fh.pop() {
+            output.push(popped);
+            assert!(popped >= prev);
+            prev = popped;
+            count += 1;
+        }
+        assert!(count == 1000);
+        input.sort();
+        output.sort();
+        assert!(input == output);
+    }
+
+    #[test]
+    fn test_push_very_many_then_pop_very_many() {
+        let mut fh: FibonacciHeap<i32> = FibonacciHeap::new();
+        for i in 0..10000 {
+            fh.push((i * i) % 2000);
+        }
+        let mut prev = i32::min_value();
+        let mut count = 0;
+        while let Some(popped) = fh.pop() {
+            assert!(popped >= prev);
+            prev = popped;
+            count += 1;
+        }
+        assert!(count == 10000);
+    }
+
+    #[test]
+    fn test_push_very_many_then_pop_very_many_with_much_overlap() {
+        let mut fh: FibonacciHeap<i32> = FibonacciHeap::new();
+        for i in 0..10000 {
+            fh.push((i * i) % 20);
+        }
+        let mut prev = i32::min_value();
+        let mut count = 0;
+        while let Some(popped) = fh.pop() {
+            assert!(popped >= prev);
+            prev = popped;
+            count += 1;
+        }
+        assert!(count == 10000);
+    }
+
+    #[test]
+    fn test_push_1000000_then_pop() {
+        let mut fh: FibonacciHeap<i32> = FibonacciHeap::new();
+        let mut input: Vec<i32> = vec![];
+        let mut output: Vec<i32> = vec![];
+        for i in 0..1000000 {
+            fh.push((i * i) % 700000);
+            input.push((i * i) % 700000);
+        }
+        let mut prev = i32::min_value();
+        while let Some(popped) = fh.pop() {
+            assert!(popped >= prev);
+            output.push(popped);
+            prev = popped;
+        }
+        input.sort();
+        output.sort();
+        assert!(input == output);
+    }
+
+    #[test]
+    fn test_push_1000000_then_pop_with_much_overlap() {
+        let mut fh: FibonacciHeap<i32> = FibonacciHeap::new();
+        let mut input: Vec<i32> = vec![];
+        let mut output: Vec<i32> = vec![];
+        for i in 0..1000000 {
+            fh.push((i * i) % 7000);
+            input.push((i * i) % 7000);
+        }
+        let mut prev = i32::min_value();
+        while let Some(popped) = fh.pop() {
+            assert!(popped >= prev);
+            output.push(popped);
+            prev = popped;
+        }
+        input.sort();
+        output.sort();
+        assert!(input == output);
+    }
+
+    #[test]
+    fn test_push_10000000_then_pop() {
+        let mut fh: FibonacciHeap<i32> = FibonacciHeap::new();
+        let mut input: Vec<i32> = vec![];
+        let mut output: Vec<i32> = vec![];
+        for i in 0..10000000 {
+            fh.push((i * i) % 700000);
+            input.push((i * i) % 700000);
+        }
+        let mut prev = i32::min_value();
+        while let Some(popped) = fh.pop() {
+            assert!(popped >= prev);
+            output.push(popped);
+            prev = popped;
+        }
+        input.sort();
+        output.sort();
+        assert!(input == output);
+    }
+
+    #[test]
+    fn test_push_10000000_then_pop_with_much_overlap() {
+        let mut fh: FibonacciHeap<i32> = FibonacciHeap::new();
+        let mut input: Vec<i32> = vec![];
+        let mut output: Vec<i32> = vec![];
+        for i in 0..10000000 {
+            fh.push((i * i) % 7000);
+            input.push((i * i) % 7000);
+        }
+        let mut prev = i32::min_value();
+        while let Some(popped) = fh.pop() {
+            assert!(popped >= prev);
+            output.push(popped);
+            prev = popped;
+        }
+        input.sort();
+        output.sort();
+        assert!(input == output);
     }
 }
